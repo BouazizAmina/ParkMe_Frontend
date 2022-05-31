84cfa973bce05e3541d1e38a93bc92af9e6723bb
BouazizAmina <ia_bouaziz@esi.dz>
2022-05-31 11:12:53 +0100

    done with login

--- app/src/main/java/com/example/packme/retrofit/const.kt
+++ app/src/main/java/com/example/packme/retrofit/const.kt
@@ -1,3 +1,3 @@
 package com.example.packme.retrofit
 
-const val url = "https://11ce-154-121-29-23.ngrok.io/"
+const val url = "https://b7f5-129-45-27-142.ngrok.io/"
--- app/src/main/java/com/example/packme/viewModel/UtilisateurModel.kt
+++ app/src/main/java/com/example/packme/viewModel/UtilisateurModel.kt
@@ -1,6 +1,7 @@
 package com.example.packme.viewModel
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.packme.entity.Utilisateur
@@ -9,8 +10,11 @@ import kotlinx.coroutines.*
 
 class UtilisateurModel : ViewModel() {
     // Les données à partager entre les fragments
-    var loading = MutableLiveData<Boolean>(false)
+    var data = MutableLiveData<String>()
+    var loading = MutableLiveData<Boolean>()
     var credentialsValidity = MutableLiveData<Boolean>()
+//    var credentialsValidity:Boolean = false
+
     var responseMessage = MutableLiveData<String>()
     val errorMessage = MutableLiveData<String>()
     val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
@@ -18,28 +22,25 @@ class UtilisateurModel : ViewModel() {
         println("err  "+throwable.message.toString())
     }
 
-    private fun onError(message: String) {
-        errorMessage.value = message
-    }
 
     fun connexion(user: Utilisateur ){
         CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-//              val  data = HashMap<String,String>()
-//            data.put("identifier",user.email)
-//            data.put("motPasse")
-                val response = Endpoint.createInstance().seConnecter(user)
-                withContext(Dispatchers.Main) {
-                    loading.value = true
-                    if (response.isSuccessful ) {
-                        credentialsValidity.value = true
-//                        responseMessage.postValue((response.message()))
-                    } else {
-                        credentialsValidity.value = false
-                       responseMessage.postValue((response.toString()))
-                    }
+            val response = Endpoint.createInstance().seConnecter(user)
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful ) {
+                    loading.value = false
+                    credentialsValidity.value = response.body() != "-1"
+//                    credentialsValidity= response.body() != "-1"
+                    data.postValue(response.body())
+                } else {
+                    onError(response.message())
                 }
+            }
         }
-
+    }
+    private fun onError(message: String) {
+        errorMessage.value = message
+        loading.value = false
     }
 
 
